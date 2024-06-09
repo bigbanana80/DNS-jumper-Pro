f4f2ee9659643890ad8c90a820203f2a254faf5b
Sepehr Aghajani <75833176+bigbanana80@users.noreply.github.com>
2024-06-09 11:52:34 +0330

    Ui elements such as dialog loads properly

--- app.py
+++ app.py
@@ -7,8 +7,23 @@ from PySide6 import QtCore, QtGui, QtWidgets
 
 class MainApp(QtWidgets.QMainWindow):
     def __init__(self):
+        # ? Start up tasks
         super(MainApp, self).__init__()
         self.ui = MainWindow.Ui_MainWindow()
+        self.ui.setupUi(self)
+
+        # ? Connections
+        self.ui.act_btn.clicked.connect(self.act_btn)
+
+    def act_btn(self):
+        self.dialog = DialogApp()
+        self.dialog.exec()
+
+
+class DialogApp(QtWidgets.QDialog):
+    def __init__(self):
+        super(DialogApp, self).__init__()
+        self.ui = Dialog.Ui_Dialog()
         self.ui.setupUi(self)
 
 
